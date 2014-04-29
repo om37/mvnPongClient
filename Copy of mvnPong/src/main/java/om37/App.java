a08653b34c999fcce53c1e11fd40d1818a50caee
Odie <Odie@SBFCFBPC>
2014-04-29 16:11:46 +0100

    Created new Maven project base and imported pong work. Once packaged, use the two batch files to launch server/client respectively

---
+++ Copy of mvnPong/src/main/java/om37/App.java
@@ -0,0 +1,15 @@
+package om37;
+
+
+import om37.Client;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        new Client().start();
+    }
+}
